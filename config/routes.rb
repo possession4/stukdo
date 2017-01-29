6960c18540252d43e89f0b1dd7eec6905a6cad0d
possession4 <himebrittny@hotmail.com>
2017-01-29 17:08:51 -0500

    Added test page to play with bootstrap panels

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
     end
   end
   devise_for :users
+
   get 'pages/home'
 
   root 'pages#home'
 
   get 'about' => 'pages#about'
+  get 'test' => 'pages#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
